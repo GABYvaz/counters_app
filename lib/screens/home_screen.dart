3fcb27e86c3cf83d05c43c6b8825b12ef0cd581a
Gaby Vazquez <gaby27anahi02@outlook.es>
2023-02-21 12:53:42 -0600

    home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,60 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 
 class HomeScreen extends StatelessWidget{
   const HomeScreen({super.key});
-@override
-State<HomeScreen> createState() => _HomeScreenState();
-}
-  
-class _HomeScreenState extends State<HomeScreen> {
-  int contador =0;
-  final TextStyle estiloTexto = const TextStyle(
-    fontSize: 30,
-    color: Colors.blueGrey,
-    fontWeight: FontWeight.bold, 
-      );
-            
+
+ final TextStyle estiloTexto = const TextStyle(
+  fontSize: 25,
+  color: Colors.red,
+  fontWeight: FontWeight.bold,
+ );
+
   @override
   Widget build(BuildContext context) {
-    int contador=0;
+
+  int contador = 0;
+
      return Scaffold(
       appBar:  AppBar( //Barra de arriba de la app
         title: const Text('Counter App'),
         centerTitle: true, //Centra el texto de la barra 
-        elevation: 10, //sombra de la barra 
-         backgroundColor: const Color.fromARGB(10, 15, 12,14),
+        elevation: 5.2, //sombra de la barra 
+        backgroundColor: const Color.fromARGB(255, 3, 21, 52),
       ),
-      backgroundColor: const Color.fromARGB(10, 15, 12,14),
-      body:  Center(
+      backgroundColor: Colors.greenAccent,
+      body: Center(
+        
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, //agarra la columna y la centra toda
+          mainAxisAlignment: MainAxisAlignment.center,         //agarra la columna y la centra toda
           children: [
-             Text(
-              'Numero de Taps:',
-              style: estiloTexto,
-              ),
-            Text('$contador',
+            Text('Numero de Taps:',
             style: estiloTexto,
             ),
+            Text(
+              '0',
+              style: estiloTexto,
+              ),
+            
           ],
         )
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-     floatingActionButton: Row(
-       children: [
-         FloatingActionButton( //Widget y propiedad del boton
-          onPressed: () {
-            setState((){
-              contador--;
-        });
-            },
-            backgroundColor: const Color.fromARGB(255, 3, 21, 52),
-            foregroundColor: const Color.fromARGB(255, 250, 15, 78),
-            child: const Icon(Icons.exposure_minus_1),
-            ),
-      ],
-     ),      
-      );
-    } 
-  }   
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        contador++;
+        print('Tocaste el boton: $contador');
+
+
+
+                         //Error porque la clase statelessWidget solo trabaja con variables const y final y el contador es cambiante
+      },
+      backgroundColor: const Color.fromARGB(255, 3, 21, 53),
+      foregroundColor: const Color.fromARGB(255,234, 184,5),
+      child: const Icon(Icons.add),
+      )
+     );
+    }
+    
+  }
